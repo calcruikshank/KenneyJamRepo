586ed748cecf882c7616b30b5b6650eac3a8ce93
Brendan Roarty <81264039+greatOmouse@users.noreply.github.com>
2023-07-23 01:41:53 -0600

    lkjafsdjkjk

---
+++ Assets/Scripts/UVPAN.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVPAN : MonoBehaviour
+{
+
+    public float Scrollx = 0.075f;
+    public float Scrolly = 0.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float OffsetX = Time.time * Scrollx;
+        float OffsetY = Time.time * Scrolly;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+
+
+    }
+}
